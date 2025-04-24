f11f419501f560b405d5f96e5713aadfcb0c923e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-24 10:27:52 -0300

    init commit

---
+++ src/main/java/com/jovi/rpgCp2/RpgCp2Application.java
@@ -0,0 +1,13 @@
+package com.jovi.rpgCp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgCp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgCp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovi/rpgCp2/RpgCp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovi.rpgCp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgCp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
